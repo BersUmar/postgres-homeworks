418b6a8711a4bd06c74f9ed1c68d6b243a33eba6
BersUmar <umar-03@inbox.ru>
2023-09-04 14:29:25 +0300

    __

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,28 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	customer_id varchar,
+	company_name varchar,
+	contact_name varchar
+
+)
+
+CREATE TABLE employees
+(
+	employee_id varchar,
+	first_name varchar,
+	last_name varchar,
+	title varchar,
+	birth_date date,
+	notes text
+
+)
+
+CREATE TABLE orders
+(
+	order_id int NOT NULL,
+	customer_id varchar (10),
+	employee_id int2,
+	order_date date,
+	ship_city varchar
+)
