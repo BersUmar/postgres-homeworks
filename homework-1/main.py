05b525beb25cadd8bfdc676303e91f5b25c12e8d
BersUmar <umar-03@inbox.ru>
2023-09-05 17:21:25 +0300

    finish hw-1

--- homework-1/main.py
+++ homework-1/main.py
@@ -17,7 +17,6 @@ with open(file='../homework-1/north_data/customers_data.csv', encoding='utf-8') 
         curs.execute('INSERT INTO customers VALUES (%s,%s,%s)', (info["customer_id"], info["company_name"], info["contact_name"]))
     connect.commit()
     curs.close()
-    connect.close()
 
 with open(file='../homework-1/north_data/employees_data.csv', encoding='utf-8') as f:
     file_reader = DictReader(f)
@@ -26,7 +25,6 @@ with open(file='../homework-1/north_data/employees_data.csv', encoding='utf-8') 
         curs.execute('INSERT INTO employees VALUES (%s,%s,%s,%s,%s,%s)', (info["employee_id"], info["first_name"], info["last_name"], info['title'], info['birth_date'], info['notes']))
     connect.commit()
     curs.close()
-    connect.close()
 
 with open(file='../homework-1/north_data/orders_data.csv.csv', encoding='utf-8') as f:
     file_reader = DictReader(f)
@@ -35,14 +33,6 @@ with open(file='../homework-1/north_data/orders_data.csv.csv', encoding='utf-8')
         curs.execute('INSERT INTO orders VALUES (%s,%s,%s)', (info["order_id"], info["customer_id"], info["employee_id"], info['order_date'], info['ship_city']))
     connect.commit()
     curs.close()
-    connect.close()
 
-    # curs.execute('SELECT * FROM customers')
-    # first = curs.fetchall()
-    # for i in first:
-    #     print(i)
+connect.close()
 
-# curs.close()
-# connect.close()
-print(curs.execute('SELECT * FROM employees'))
-print(curs.execute('SELECT * FROM orders'))
